8a636ec0db3ad4cc9fd1284d94e35848a8796ad7
eddio0141 <eddio0141@gmail.com>
2021-11-05 10:19:16 +0000

    hltas file content displays

--- src/guis/main.rs
+++ src/guis/main.rs
@@ -1,7 +1,7 @@
-use std::{path::PathBuf};
+use std::{fs, path::PathBuf};
 
 use eframe::{
-    egui::{self, menu, Color32},
+    egui::{self, menu, Color32, Label, Sense},
     epi,
 };
 use hltas::HLTAS;
@@ -18,20 +18,33 @@ fn hltas_to_str(hltas: &HLTAS) -> String {
     String::new()
 }
 
-struct Tab<'a> {
+struct Tab {
     title: String,
     path: Option<PathBuf>,
-    hltas: HLTAS<'a>,
+    raw_content: String,
 }
 
-impl<'a> Tab<'a> {
-    fn open_path(path: PathBuf) -> Self {
-        Self {
-            // TODO error check?
-            // this is file so it should be
-            title: path.file_name().unwrap().to_str().unwrap().to_owned(),
-            path: Some(path),
-            ..Default::default()
+impl Tab {
+    fn open_path(path: PathBuf) -> Result<Self, String> {
+        if let Ok(file_content) = fs::read_to_string(&path) {
+            match HLTAS::from_str(&file_content) {
+                Ok(_) => {}
+                // TODO better error handling
+                Err(_) => {
+                    return Err("Error, can't open the file as hltas file".to_owned());
+                }
+            }
+
+            Ok(Self {
+                // TODO error check?
+                // this is file so it should be
+                title: path.file_name().unwrap().to_str().unwrap().to_owned(),
+                path: Some(path.clone()),
+                raw_content: file_content,
+            })
+        } else {
+            // TODO better error
+            Err("Error, can't open the file".to_owned())
         }
     }
 
@@ -40,13 +53,13 @@ impl<'a> Tab<'a> {
     }
 }
 
-impl<'a> Default for Tab<'a> {
+impl Default for Tab {
     // TODO translation support
     fn default() -> Self {
         Self {
             title: "New file".to_owned(),
             path: None,
-            hltas: Default::default(),
+            raw_content: hltas_to_str(&HLTAS::default()),
         }
     }
 }
@@ -54,16 +67,17 @@ impl<'a> Default for Tab<'a> {
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 // #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 // #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
-pub struct MainGUI<'a> {
-    tabs: Vec<Tab<'a>>,
+pub struct MainGUI {
+    tabs: Vec<Tab>,
     // might have a chance to not have any tabs opened
-    current_tab: Option<usize>,
+    // TODO use direct reference
+    current_tab_index: Option<usize>,
 }
 
-impl<'a> MainGUI<'a> {
+impl MainGUI {
     pub fn new_file(&mut self) {
         self.tabs.push(Tab::new_file());
-        self.current_tab = Some(self.tabs.len() - 1);
+        self.current_tab_index = Some(self.tabs.len() - 1);
     }
 
     pub fn open_file(&mut self) {
@@ -71,22 +85,26 @@ impl<'a> MainGUI<'a> {
             .add_filter("HLTAS Files", &["hltas", "txt"])
             .show_open_single_file()
         {
-            self.tabs.push(Tab::open_path(pathbuf));
+            // TODO error handling here
+            if let Ok(tab) = Tab::open_path(pathbuf) {
+                self.tabs.push(tab);
+                self.current_tab_index = Some(self.tabs.len() - 1);
+            }
         }
     }
 }
 
-impl<'a> Default for MainGUI<'a> {
+impl Default for MainGUI {
     // first time opened will always show a new tab
     fn default() -> Self {
         Self {
             tabs: vec![Tab::default()],
-            current_tab: Some(0),
+            current_tab_index: Some(0),
         }
     }
 }
 
-impl<'a> epi::App for MainGUI<'a> {
+impl epi::App for MainGUI {
     fn setup(
         &mut self,
         _ctx: &egui::CtxRef,
@@ -140,10 +158,20 @@ impl<'a> epi::App for MainGUI<'a> {
             // tabs
             let mut stale_tabs: Vec<usize> = Vec::new();
             ui.horizontal(|ui| {
+                let mut new_index: Option<usize> = None;
                 for (index, tab) in self.tabs.iter().enumerate() {
                     // tab design
                     ui.group(|ui| {
-                        ui.label(&tab.title);
+                        // if label is clicked, switch to that one
+                        if ui
+                            .add(Label::new(&tab.title).sense(Sense::click()))
+                            .clicked()
+                        {
+                            // FIXME not sure why I can't do this
+                            // self.current_tab_index = Some(index);
+                            new_index = Some(index);
+                        }
+
                         let close_button = egui::Button::new("x")
                             .small()
                             .text_color(Color32::from_rgb(255, 0, 0));
@@ -154,6 +182,10 @@ impl<'a> epi::App for MainGUI<'a> {
                         }
                     });
                 }
+
+                if let Some(_) = new_index {
+                    self.current_tab_index = new_index;
+                }
             });
 
             stale_tabs.reverse();
@@ -162,10 +194,22 @@ impl<'a> epi::App for MainGUI<'a> {
             for index in stale_tabs {
                 self.tabs.remove(index);
             }
+
+            // fix index if its out of bounds
+            if let Some(index) = self.current_tab_index {
+                if self.tabs.len() == 0 {
+                    self.current_tab_index = None;
+                } else if index >= self.tabs.len() {
+                    self.current_tab_index = Some(self.tabs.len() - 1);
+                }
+            }
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // ui.text_edit_multiline(&mut self.raw_content);
+            if let Some(current_tab_index) = self.current_tab_index {
+                let current_tab = &mut self.tabs[current_tab_index];
+                ui.text_edit_multiline(&mut current_tab.raw_content);
+            }
         });
     }
 

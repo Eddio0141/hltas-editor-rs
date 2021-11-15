205b2b63957aca1e6464aaa97b0f2fc348d11c40
eddio0141 <eddio0141@gmail.com>
2021-11-15 23:54:15 +0000

    added helpers

---
+++ src/helpers/hltas/mod.rs
@@ -0,0 +1,12 @@
+use hltas::HLTAS;
+
+pub fn hltas_to_str(hltas: &HLTAS) -> String {
+    let mut file_u8: Vec<u8> = Vec::new();
+    hltas.to_writer(&mut file_u8).unwrap();
+
+    // always has to work
+    if let Ok(content) = String::from_utf8(file_u8) {
+        return content;
+    }
+    String::new()
+}
---
+++ src/helpers/mod.rs
@@ -0,0 +1,2 @@
+pub mod hltas;
+pub mod egui;

e06d46a15626167cc51697f04b6c124bf3c9d015
eddio0141 <eddio0141@gmail.com>
2021-11-20 02:03:51 +0000

    hltas_to_str panics if fails which it wont

--- src/helpers/hltas/mod.rs
+++ src/helpers/hltas/mod.rs
@@ -6,9 +6,9 @@ pub fn hltas_to_str(hltas: &HLTAS) -> String {
     let mut file_u8: Vec<u8> = Vec::new();
     hltas.to_writer(&mut file_u8).unwrap();
 
-    // always has to work
     if let Ok(content) = String::from_utf8(file_u8) {
         return content;
     }
-    String::new()
+    // shouldn't be possible for this to happen
+    panic!("unable to convert hltas to string");
 }

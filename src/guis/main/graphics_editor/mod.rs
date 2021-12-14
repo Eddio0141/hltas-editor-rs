c97b0cc7aa190d1c7fd3900b279afd42dfefea9b
Eddio0141 <eddio0141@gmail.com>
2021-12-14 22:00:18 +0000

    moved graphics_editor.rs to its own folder

--- src/guis/main/graphics_editor/mod.rs
+++ src/guis/main/graphics_editor/mod.rs
@@ -1,3 +1,5 @@
+mod yaw_pitch_menu;
+
 use std::num::NonZeroU32;
 
 use hltas::types::{
@@ -6,7 +8,7 @@ use hltas::types::{
     Seeds, StrafeDir, StrafeSettings, StrafeType, Times, VectorialStrafingConstraints,
 };
 use imgui::{
-    CollapsingHeader, ComboBox, Drag, InputFloat, InputText, Selectable, Slider, StyleColor, Ui,
+    CollapsingHeader, ComboBox, Drag, InputFloat, InputText, Selectable, StyleColor, Ui,
 };
 
 use crate::{
@@ -16,6 +18,8 @@ use crate::{
     },
     helpers::hltas::button_to_str,
 };
+
+use self::yaw_pitch_menu::show_yaw_pitch_menu;
 
 use super::{
     cmd_editor::cmd_editor_ui,
@@ -235,131 +239,8 @@ pub fn show_graphics_editor(ui: &Ui, tab: &mut HLTASFileTab) {
                     };
 
                     // yaw pitch menu
-                    let yaw_pitch_edited = ui.group(|| {
-                        let yaw_pitch_setter_width = ui.window_content_region_width() * 0.2;
-
-                        let yaw_editor = |yaw| {
-                            let x_button_clicked =
-                                show_x_button(ui, &format!("yaw_set_close{}", i));
-
-                            ui.same_line();
-
-                            ui.set_cursor_screen_pos([
-                                yaw_pitch_menu_offset,
-                                ui.cursor_screen_pos()[1],
-                            ]);
-
-                            let item_width_token = ui.push_item_width(yaw_pitch_setter_width);
-                            let yaw_set_changed = Drag::new(format!("##yaw_set{}", i))
-                                .speed(0.1)
-                                .display_format("yaw: %f")
-                                .build(ui, yaw);
-                            item_width_token.pop(ui);
-
-                            if x_button_clicked {
-                                None
-                            } else {
-                                Some(yaw_set_changed)
-                            }
-                        };
-                        let yaw_button = |disabled, auto_movement: &mut Option<AutoMovement>| {
-                            // ui.disabled returns nothing so hacky work around
-                            let mut edited = false;
-                            ui.disabled(disabled, || {
-                                ui.set_cursor_screen_pos([
-                                    yaw_pitch_menu_offset,
-                                    ui.cursor_screen_pos()[1],
-                                ]);
-
-                                if ui.button_with_size(
-                                    format!("{}##yaw_set_button{}", "set yaw", i),
-                                    [yaw_pitch_setter_width, 0.0],
-                                ) {
-                                    *auto_movement = Some(AutoMovement::SetYaw(0.0));
-                                    edited = true;
-                                }
-                            });
-                            Some(edited)
-                        };
-
-                        let edited_yaw = match &mut framebulk.auto_actions.movement {
-                            Some(auto_movement) => match auto_movement {
-                                AutoMovement::SetYaw(yaw) => yaw_editor(yaw),
-                                AutoMovement::Strafe(strafe_settings) => {
-                                    match &mut strafe_settings.dir {
-                                        StrafeDir::Yaw(yaw) => yaw_editor(yaw),
-                                        StrafeDir::Line { yaw } => yaw_editor(yaw),
-                                        _ => yaw_button(true, &mut framebulk.auto_actions.movement),
-                                    }
-                                }
-                            },
-                            None => {
-                                // show yaw button
-                                yaw_button(false, &mut framebulk.auto_actions.movement)
-                            }
-                        };
-
-                        let edited_yaw = match edited_yaw {
-                            Some(edited_yaw) => edited_yaw,
-                            None => {
-                                framebulk.auto_actions.movement = None;
-                                true
-                            }
-                        };
-
-                        let edited_pitch = match &mut framebulk.pitch {
-                            Some(pitch) => {
-                                let pitch_x_clicked =
-                                    show_x_button(ui, &format!("pitch_set_close{}", i));
-
-                                ui.same_line();
-
-                                ui.set_cursor_screen_pos([
-                                    yaw_pitch_menu_offset,
-                                    ui.cursor_screen_pos()[1],
-                                ]);
-
-                                let item_width_token = ui.push_item_width(yaw_pitch_setter_width);
-                                let pitch_set_changed =
-                                    Slider::new(format!("##pitch_set{}", i), -89.0, 89.0)
-                                        .display_format("pitch: %f")
-                                        .build(ui, pitch);
-                                item_width_token.pop(ui);
-
-                                if pitch_x_clicked {
-                                    None
-                                } else {
-                                    Some(pitch_set_changed)
-                                }
-                            }
-                            None => {
-                                ui.set_cursor_screen_pos([
-                                    yaw_pitch_menu_offset,
-                                    ui.cursor_screen_pos()[1],
-                                ]);
-
-                                let pitch_set_button_clicked = ui.button_with_size(
-                                    format!("{}##pitch_set_button{}", "set pitch", i),
-                                    [yaw_pitch_setter_width, 0.0],
-                                );
-
-                                if pitch_set_button_clicked {
-                                    framebulk.pitch = Some(0.0);
-                                }
-
-                                Some(pitch_set_button_clicked)
-                            }
-                        };
-
-                        let edited_pitch = match edited_pitch {
-                            Some(edited_pitch) => edited_pitch,
-                            None => {
-                                framebulk.pitch = None;
-                                true
-                            }
-                        };
-
-                        edited_yaw || edited_pitch
+                    let yaw_pitch_menu_edited = ui.group(|| {
+                        show_yaw_pitch_menu(&ui, yaw_pitch_menu_offset, framebulk, &i.to_string())
                     });
 
                     ui.same_line();
@@ -859,7 +740,7 @@ pub fn show_graphics_editor(ui: &Ui, tab: &mut HLTASFileTab) {
                         use_changed || attack1_changed || attack2_changed || reload_changed
                     });
 
-                    yaw_pitch_edited
+                    yaw_pitch_menu_edited
                         || strafe_menu_edited
                         || jump_menu_edited
                         || duck_menu_edited

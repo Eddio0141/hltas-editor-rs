26e1e9360b3567eb23509b33c07598c2a304c846
Eddio0141 <eddio0141@gmail.com>
2021-12-15 03:11:35 +0000

    commented out unused function and variable

--- src/helpers/hltas/frametime.rs
+++ src/helpers/hltas/frametime.rs
@@ -1,12 +1,12 @@
 pub const MIN: f32 = 0.25;
 // TODO look into this a bit more
-pub const MAX: f32 = 0.0000000000001;
+// pub const MAX: f32 = 0.0000000000001;
 pub const MAX_STRAFE: f32 = 0.001;
 
-pub fn is_0ms(frametime: &str) -> bool {
-    match frametime.parse::<f32>() {
-        // TODO knowing a way to actually check this?
-        Ok(frametime) => frametime < 0.000001,
-        Err(_) => false,
-    }
-}
+// pub fn is_0ms(frametime: &str) -> bool {
+//     match frametime.parse::<f32>() {
+//         // TODO knowing a way to actually check this?
+//         Ok(frametime) => frametime < 0.000001,
+//         Err(_) => false,
+//     }
+// }

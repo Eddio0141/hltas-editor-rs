7fc48d42f655edd109a2c6dc1df76908c7a3266b
Eddio0141 <eddio0141@gmail.com>
2021-12-22 13:41:35 +0000

    is_0ms properly checks now

--- src/helpers/hltas/frametime.rs
+++ src/helpers/hltas/frametime.rs
@@ -4,8 +4,7 @@ pub const MAX_STRAFE: f32 = 0.001;
 
 // pub fn is_0ms(frametime: &str) -> bool {
 //     match frametime.parse::<f32>() {
-//         // TODO knowing a way to actually check this?
-//         Ok(frametime) => frametime < 0.000001,
+//         Ok(frametime) => frametime < 0.001,
 //         Err(_) => false,
 //     }
 // }

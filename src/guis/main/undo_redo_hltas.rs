12c22f529a9d27ddfb27c767f54ed83f4990e6a6
Eddio0141 <eddio0141@gmail.com>
2021-12-31 01:41:06 +0000

    cleaned up action handler

--- src/guis/main/undo_redo_hltas.rs
+++ src/guis/main/undo_redo_hltas.rs
@@ -12,6 +12,46 @@ enum Action {
     },
 }
 
+impl Action {
+    // TODO is there a better way to implement this (the hltas and tab_menu_data)
+
+    /// Takes action to the hltas file depending on what enum is selected
+    ///
+    /// * Returns a reverse of what action was taken in a new Action instance
+    fn take_action(&self, hltas: &mut HLTAS, tab_menu_data: &mut HLTASMenuState) -> Self {
+        match self {
+            Action::DeleteLine {
+                indexes_and_lines: lines_and_indexes,
+            } => {
+                for (i, line) in lines_and_indexes {
+                    if hltas.lines.is_empty() {
+                        tab_menu_data.push_hltas_line(&line);
+                        hltas.lines.push(line.to_owned());
+                    } else {
+                        tab_menu_data.insert_hltas_line(*i, &line);
+                        hltas.lines.insert(*i, line.to_owned());
+                    }
+                }
+
+                Action::AddLine {
+                    indexes: lines_and_indexes.into_iter().map(|(i, _)| *i).collect(),
+                }
+            }
+            Action::AddLine { indexes } => {
+                let mut indexes_and_lines = Vec::new();
+
+                for i in indexes.into_iter().rev() {
+                    tab_menu_data.remove_line_at_index(*i);
+                    indexes_and_lines.push((*i, hltas.lines[*i].to_owned()));
+                    hltas.lines.remove(*i);
+                }
+
+                Action::DeleteLine { indexes_and_lines }
+            }
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct UndoRedoHandler {
     undo_stack: Vec<Action>,
@@ -26,38 +66,10 @@ impl UndoRedoHandler {
         }
     }
 
-    // TODO is there a better way to implement this (the hltas and tab_menu_data)
     pub fn undo(&mut self, hltas: &mut HLTAS, tab_menu_data: &mut HLTASMenuState) {
         if let Some(undo_action) = self.undo_stack.pop() {
-            self.redo_stack.push(match undo_action {
-                Action::DeleteLine { indexes_and_lines: lines_and_indexes } => {
-                    for (i, line) in &lines_and_indexes {
-                        if hltas.lines.is_empty() {
-                            tab_menu_data.push_hltas_line(&line);
-                            hltas.lines.push(line.to_owned());
-                        } else {
-                            tab_menu_data.insert_hltas_line(*i, &line);
-                            hltas.lines.insert(*i, line.to_owned());
-                        }
-                    }
-
-                    Action::AddLine {
-                        indexes: lines_and_indexes.into_iter().map(|(i, _)| i).collect(),
-                    }
-                }
-                Action::AddLine { indexes } => {
-                    let mut indexes_and_lines = Vec::new();
-
-                    for i in indexes.into_iter().rev() {
-                        tab_menu_data.remove_line_at_index(i);
-                        indexes_and_lines.push((i, hltas.lines[i].to_owned()));
-                        hltas.lines.remove(i);
-                    }
-
-
-                    Action::DeleteLine { indexes_and_lines }
-                }
-            });
+            self.redo_stack
+                .push(undo_action.take_action(hltas, tab_menu_data));
 
             tab_menu_data.got_modified();
         }
@@ -65,35 +77,8 @@ impl UndoRedoHandler {
 
     pub fn redo(&mut self, hltas: &mut HLTAS, tab_menu_data: &mut HLTASMenuState) {
         if let Some(redo_action) = self.redo_stack.pop() {
-            self.undo_stack.push(match redo_action {
-                Action::DeleteLine { indexes_and_lines: lines_and_indexes } => {
-                    for (i, line) in &lines_and_indexes {
-                        if hltas.lines.is_empty() {
-                            tab_menu_data.push_hltas_line(&line);
-                            hltas.lines.push(line.to_owned());
-                        } else {
-                            tab_menu_data.insert_hltas_line(*i, &line);
-                            hltas.lines.insert(*i, line.to_owned());
-                        }
-                    }
-
-                    Action::AddLine {
-                        indexes: lines_and_indexes.into_iter().map(|(i, _)| i).collect(),
-                    }
-                }
-                Action::AddLine { indexes } => {
-                    let mut indexes_and_lines = Vec::new();
-
-                    for i in indexes.into_iter().rev() {
-                        tab_menu_data.remove_line_at_index(i);
-                        indexes_and_lines.push((i, hltas.lines[i].to_owned()));
-                        hltas.lines.remove(i);
-                    }
-
-
-                    Action::DeleteLine { indexes_and_lines }
-                }
-            });
+            self.undo_stack
+                .push(redo_action.take_action(hltas, tab_menu_data));
 
             tab_menu_data.got_modified();
         }

3166fa5ad01cd63ad6c9254dcb973a449f4047c4
Eddio0141 <eddio0141@gmail.com>
2022-01-24 01:23:48 +0000

    added scuffed undo redo

--- src/guis/main/cmd_editor.rs
+++ src/guis/main/cmd_editor.rs
@@ -1,10 +1,48 @@
+use hltas::types::FrameBulk;
 use imgui::{InputText, Ui};
 
 use crate::helpers::locale::locale_lang::LocaleLang;
+
+use super::graphics_editor::framebulk_editor::FramebulkEditorMiscData;
 
 pub fn show_cmd_editor(ui: &Ui, cmds: &mut String, label: &str, locale_lang: &LocaleLang) -> bool {
     // TODO
     InputText::new(ui, label, cmds)
         .hint(locale_lang.get_string_from_id("commands"))
         .build()
+}
+
+pub fn show_cmd_editor_undo_redo_line(
+    ui: &Ui,
+    framebulk: &mut FrameBulk,
+    label: &str,
+    misc_data: FramebulkEditorMiscData,
+    index: usize,
+) -> bool {
+    // TODO
+    if let Some(cmds) = &mut framebulk.console_command {
+        let (locale_lang, tab_menu_data, undo_redo_handler) = (
+            misc_data.options.locale_lang(),
+            misc_data.tab_menu_data,
+            misc_data.undo_redo_handler,
+        );
+
+        let edited = InputText::new(ui, label, cmds)
+            .hint(locale_lang.get_string_from_id("commands"))
+            .build();
+
+        // TODO fix this
+        if ui.is_item_activated() {
+            println!("activated");
+            tab_menu_data.set_framebulk_edit_backup(framebulk, index)
+        }
+        if ui.is_item_deactivated_after_edit() {
+            println!("deactivated");
+            tab_menu_data.set_undo_point_with_backup(undo_redo_handler)
+        }
+
+        edited
+    } else {
+        false
+    }
 }
--- src/guis/main/graphics_editor/command_editor.rs
+++ src/guis/main/graphics_editor/command_editor.rs
@@ -1,6 +1,7 @@
+use hltas::types::Line;
 use imgui::Ui;
 
-use crate::guis::main::cmd_editor::show_cmd_editor;
+use crate::guis::main::cmd_editor::{show_cmd_editor, show_cmd_editor_undo_redo_line};
 
 use super::framebulk_editor::{FramebulkEditor, FramebulkEditorMiscData, FramebulkInfo};
 
@@ -15,21 +16,27 @@ impl FramebulkEditor for CommandEditor {
         index: usize,
     ) -> bool {
         let framebulk = hltas_info.framebulk;
-        let options = misc_data.options;
+        let (tab_menu_data, options, undo_redo_handler) = (
+            misc_data.tab_menu_data,
+            misc_data.options,
+            misc_data.undo_redo_handler,
+        );
 
         let locale_lang = options.locale_lang();
 
         ui.text(locale_lang.get_string_from_id("commands"));
-        match &mut framebulk.console_command {
-            Some(cmds) => show_cmd_editor(
+        match framebulk.console_command {
+            Some(_) => show_cmd_editor_undo_redo_line(
                 ui,
-                cmds,
+                framebulk,
                 &format!("##command_menu_cmds{}", index),
-                locale_lang,
+                FramebulkEditorMiscData::new(tab_menu_data, options, undo_redo_handler),
+                index,
             ),
             None => {
                 let button_clicked = ui.button(format!("set commands##{}", index));
                 if button_clicked {
+                    undo_redo_handler.edit_line(Line::FrameBulk(framebulk.to_owned()), index);
                     framebulk.console_command = Some(String::from(""));
                 }
                 button_clicked
